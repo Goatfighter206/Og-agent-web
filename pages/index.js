269498e2907c1a61610ccfd8b576b4424c4f6f56
Goatfighter206 <williamson.bt@gmail.com>
2025-11-06 10:48:39 -0800

    Initial commit of Next.js starter code for OG-agent website.

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Welcome to the OG Agent Interactive Experience!</h1>
+      <p>Interact with our 3D robot.</p>
+    </div>
+  );
+};
+
+export default IndexPage;
